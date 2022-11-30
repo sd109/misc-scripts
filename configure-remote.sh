550cd0b817b42f25c74ea8826544f65898f5f663
Scott Davidson <scott@stackhpc.com>
2022-11-30 15:38:02 +0000

    Uncomment all sections

--- configure-remote.sh
+++ configure-remote.sh
@@ -6,41 +6,45 @@ if [ -z $HOST ]; then
     exit
 fi
 
-# # Configure bind mounts for up/down arrows to improve shell history searching
-# REMOTE_HAS_INPUTRC=False
-# ssh -q $HOST [[ -f .inputrc ]] && REMOTE_HAS_INPUTRC=True
-# if [ $REMOTE_HAS_INPUTRC == False ]; then
-#     echo "Creating .inputrc file"
-#     ssh $HOST 'echo \"\\e[A\": history-search-backward >> .inputrc'
-#     ssh $HOST 'echo \"\\e[B\": history-search-forward >> .inputrc'
-# fi
+# Configure bind mounts for up/down arrows to improve shell history searching
+REMOTE_HAS_INPUTRC=False
+ssh -q $HOST [[ -f .inputrc ]] && REMOTE_HAS_INPUTRC=True
+if [ $REMOTE_HAS_INPUTRC == False ]; then
+    echo "Creating .inputrc file"
+    ssh $HOST 'echo \"\\e[A\": history-search-backward >> .inputrc'
+    ssh $HOST 'echo \"\\e[B\": history-search-forward >> .inputrc'
+fi
 
-# echo "Installing useful packages"
-# USE_DNF=False
-# PACKAGES="git nano tmux"
-# ssh -q $HOST "which dnf 2>/dev/null >/dev/null" && USE_DNF=True 
-# if [USE_DNF=False]; then
-#     ssh $HOST "sudo dnf -y install $PACKAGES"
-# else
-#     ssh $HOST "sudo apt update && sudo apt -y install $PACKAGES"
-# fi
+echo "Installing useful packages:"
+USE_DNF=False
+PACKAGES="git nano tmux"
+echo $PACKAGES
+ssh -q $HOST "which dnf 2>/dev/null >/dev/null" && USE_DNF=True 
+if [USE_DNF=False]; then
+    ssh $HOST "sudo dnf -y install $PACKAGES"
+else
+    ssh $HOST "sudo apt update && sudo apt -y install $PACKAGES"
+fi
 
-# # Configure git on remote
-# REMOTE_HAS_GIT_KEY=False
-# ssh -q $HOST [[ -f .ssh/github_id_rsa ]] && REMOTE_HAS_GIT_KEY=True
-# if [ $REMOTE_HAS_GIT_KEY == False ]; then 
-#     echo "Copying git ssh key"
-#     scp ~/.ssh/keys/github_id_rsa $HOST:.ssh/
-#     echo "Adding github.com to ssh config"
-#     ssh $HOST "echo -e '\nHost github.com\n HostName github.com\n IdentityFile ~/.ssh/github_id_rsa\n' >> ~/.ssh/config"
-#     ssh $HOST "chmod 644 .ssh/config"
-#     echo "Setting git user.name and user.email"
-#     ssh $HOST "git config --global user.name 'Scott Davidson'"
-#     ssh $HOST "git config --global user.email scott@stackhpc.com"
-# fi
+# Configure git on remote
+REMOTE_HAS_GIT_KEY=False
+ssh -q $HOST [[ -f .ssh/github_id_rsa ]] && REMOTE_HAS_GIT_KEY=True
+if [ $REMOTE_HAS_GIT_KEY == False ]; then 
 
-# echo "Copying tmux config"
-# scp ~/.tmux.conf $HOST:
+    echo "Copying git ssh key"
+    scp ~/.ssh/keys/github_id_rsa $HOST:.ssh/
+
+    echo "Adding github.com to ssh config"
+    ssh $HOST "echo -e '\nHost github.com\n HostName github.com\n IdentityFile ~/.ssh/github_id_rsa\n' >> ~/.ssh/config"
+    ssh $HOST "chmod 644 .ssh/config"
+
+    echo "Copying over .gitconfig"
+    scp ~/.gitconfig $HOST:
+
+fi
+
+echo "Copying tmux config"
+scp ~/.tmux.conf $HOST:
 
 echo "Adding extra settings to .bashrc:"
 #Increase max size of .bash_history file
