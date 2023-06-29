fa870801d68a594974f557e25b737aa555fc8d66
Scott Davidson <scott@stackhpc.com>
2023-06-29 11:29:58 +0100

    Script improvements

--- bootstrap-local.sh
+++ bootstrap-local.sh
@@ -1,3 +1,10 @@
+
+# Check for single command line arg
+if [[ -z "$1" || ! -z "$2" ]]; then
+    echo "Comma-separated ansible playbook tags required as sole script arg"
+    exit 1
+fi
+
 if [[ $(cat /etc/os-release) =~ ubuntu ]]; then
     sudo apt install python3-venv
 else
@@ -9,3 +16,4 @@ source venv/bin/activate
 pip install -U pip
 pip install ansible
 echo "localhost ansible_connection=local" > inventory
+ansible-playbook -i inventory configure-host.yml -t $1
