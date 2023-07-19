8f17d21d2980828fee9d39a265f8afbd1d3c2e69
Scott Davidson <scott@stackhpc.com>
2023-07-19 17:56:08 +0100

    Add ssh tunnel check

--- macos-socks-proxy.sh
+++ macos-socks-proxy.sh
@@ -5,11 +5,19 @@ PROXY_HOST="127.0.0.1"
 PROXY_PORT=9090
 
 if [[ $1 == "on" ]]; then
+        
+    # Check that ssh command is running
+    if [[ $(ps aux | grep ssh | grep $PROXY_PORT | wc -l | xargs) != 1 ]]; then
+        echo "ERROR: No ssh tunnel detected on port $PROXY_PORT"
+        exit 1
+    fi
     sudo networksetup -setsocksfirewallproxy "$PROXY_INTERFACE" $PROXY_HOST $PROXY_PORT
     echo "SOCKS proxy enabled"
+
 elif [[ $1 == "off" ]]; then
     sudo networksetup -setsocksfirewallproxystate "$PROXY_INTERFACE" off
     echo "SOCKS proxy disabled"
+
 elif [[ $1 == "status" ]]; then
     echo "======================================================"
     echo "Network Services:"
@@ -21,6 +29,7 @@ elif [[ $1 == "status" ]]; then
     echo "======================================================"
     networksetup -getsocksfirewallproxy "$PROXY_INTERFACE"
     echo
+
 else
     echo "`basename $0` toggles SOCKS proxy settings on OS X"
     echo
